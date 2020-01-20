18aecffc72af6a746c0ab768cb61385aacab8ee8
Graham Hewett <hewettgraham@gmail.com>
2020-01-20 01:15:44 +0000

    condense playlist grids

--- src/frontend/views/GenresGrid.js
+++ src/frontend/views/GenresGrid.js
@@ -1,20 +1,23 @@
 import React from "react";
-import Genres from "./Genres";
 import "./genres-grid.css";
+
+const genres = [
+  { id: "rock", genre: "Rock", playlistId: "2ihY1sy2Eask1kLJME0UhG" },
+  { id: "pop", genre: "Pop", playlistId: "37i9dQZF1DXcZDD7cfEKhW" },
+  { id: "hiphop", genre: "Hip-Hop", playlistId: "4VHnyY41iIRJa5ykDj3gzU" },
+  { id: "jazz", genre: "Jazz", playlistId: "79eC7PKQmhSsneKE5PpwtM" },
+  { id: "classical", genre: "Classical", playlistId: "1kGtBpJnR0bPWX4JXi5wUo" },
+  { id: "blues", genre: "Blues", playlistId: "37i9dQZF1DXeaMD6NdSui3" }
+];
 
 const GenresGrid = props => {
   return (
     <div className="section-genresGrid">
       <h1> Looking for music ? </h1>
       <div className="genre-container">
-        {Genres.map((genre, i) => {
+        {genres.map((genre, i) => {
           return (
-            <div
-              key={i}
-              className={[
-                'genre-item',
-                genre.playlistId === props.selectedGenre ? 'is-selected' : ''
-              ].join(' ')}
+            <div key={i} className='genre-item'
               onClick={() => props.onClick(genre.playlistId)}
             >
               <img src={require(`./images/${genre.id}.jpg`)} alt="genre" />
