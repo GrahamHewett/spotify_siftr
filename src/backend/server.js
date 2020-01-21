6686a24dcc1b29645c04d403ba50b52d075a4a3f
Graham Hewett <hewettgraham@gmail.com>
2020-01-21 10:57:07 +0000

    correct build folder relative path for deployment

--- src/backend/server.js
+++ src/backend/server.js
@@ -41,10 +41,10 @@ app.get('/callback', function(req, res) {
   })
 })
 
-app.use(express.static(path.join(__dirname, '../../../build')));
+app.use(express.static(path.join(__dirname, '../../build')));
 
 app.get('*', function (req, res) {
-  res.sendFile(path.join(__dirname, '../../../build', 'index.html'));
+  res.sendFile(path.join(__dirname, '../../build', 'index.html'));
 });
 
 console.log(`Listening on port ${port}. Go /login to initiate authentication flow.`)
